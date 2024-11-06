b36080570d5186728228015ce7a5c3a2fc1e0daa
Luke Oliff <luke@lukeoliff.com>
2024-11-06 20:29:55 +0000

    feat: random scream

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/lukeocodes/go-scream
 
-go 1.21
+go 1.22.0
+
+toolchain go1.23.1
--- main.go
+++ main.go
@@ -2,9 +2,19 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"math/rand/v2"
 )
 
 func main() {
-	fmt.Printf("%v\n", time.Now())
+	numAs := 1 + rand.IntN(20)  // Random number of A's (1-50)
+	numHs := 1 + rand.IntN(100) // Random number of H's (1-50)
+
+	scream := "A"
+	for i := 1; i < numAs; i++ {
+		scream += "A"
+	}
+	for i := 0; i < numHs; i++ {
+		scream += "H"
+	}
+	fmt.Println(scream)
 }
